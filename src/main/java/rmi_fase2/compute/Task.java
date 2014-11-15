dd68f1dd26c21e2d3152f1b1f3b1b1cb4c1936a7
felipewmartins <felipewmartins@gmail.com>
2014-11-15 17:59:49 -0300

    Novas mudanças

---
+++ src/main/java/rmi_fase2/compute/Task.java
@@ -0,0 +1,5 @@
+package rmi_fase2.compute;
+
+public interface Task<T> {
+  T execute();
+}
--- src/main/java/rmi_fase2/interfaces/Teste.java
+++
@@ -1,8 +0,0 @@
-package rmi_fase2.interfaces;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Teste extends Remote {
-  String teste() throws RemoteException;
-}
