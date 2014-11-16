90a55f60caae64160a2d4262c6101bc529c16e78
felipewmartins <felipewmartins@gmail.com>
2014-11-16 10:56:34 -0300

    Cliente

---
+++ src/main/java/rmi_fase2/client/Cliente.java
@@ -0,0 +1,19 @@
+package rmi_fase2.client;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import rmi_fase2.compute.Compute;
+
+public class Cliente {
+
+  public static void main(String[] args) {
+    try {
+      String name = "compute";
+      Registry registry = LocateRegistry.getRegistry(2001);
+      Compute comp = (Compute)registry.lookup(name);
+    } catch (Exception e) {
+      // TODO: handle exception
+    }
+  }
+}
