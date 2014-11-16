85b663ad9c45a21eaf7c2e2dbeda4bde6113935e
felipewmartins <felipewmartins@gmail.com>
2014-11-16 08:55:33 -0300

    Ajuste no servidor rmi

--- src/main/java/rmi_fase2/server/ComputeEngine.java
+++ src/main/java/rmi_fase2/server/ComputeEngine.java
@@ -1,5 +1,6 @@
 package rmi_fase2.server;
 
+import java.io.Serializable;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -8,25 +9,34 @@ import java.rmi.server.UnicastRemoteObject;
 import rmi_fase2.compute.Compute;
 import rmi_fase2.compute.Task;
 
-public class ComputeServer implements Compute {
-  
-  public  ComputeServer() {
+public class ComputeEngine implements Compute, Serializable {
+
+  private static final long serialVersionUID = -2163726111780263507L;
+
+  public  ComputeEngine() {
    super(); // TODO Auto-generated constructor stub
   }
 
   @Override
   public <T> T executeTask(Task<T> t) throws RemoteException {
     // TODO Auto-generated method stub
-    return null;
+    return t.execute();
   }
   
   public static void main(String[] args) {
-    if (System.getSecurityManager() == null) {
-      System.setSecurityManager(new SecurityManager());
+    /*if (System.getSecurityManager() == null) {
+      System.setSecurityManager(new SecurityManager()); //instala a segurança da aplicação
+    }*/
+    try {
+      LocateRegistry.createRegistry(1099);
+      System.out.println("RMI registrado na porta 1099");
+    } catch (Exception e) {
+      // TODO: handle exception
+      e.printStackTrace();
     }
     try {
       String name = "Compute";
-      Compute engine = new ComputeServer();
+      Compute engine = new ComputeEngine();
       Compute stub =
           (Compute) UnicastRemoteObject.exportObject(engine, 0);
       Registry registry = LocateRegistry.getRegistry();
