5961c9025078a256a9f6ab12cd70129becaf3bb2
Caio H <caiohenriqueads@gmail.com>
2014-11-16 23:28:12 -0300

    Alteracoes no metodo de gerar log

--- src/main/java/rmi_fase2/controle/GeraTxt.java
+++ src/main/java/rmi_fase2/controle/GeraTxt.java
@@ -11,6 +11,7 @@ public class GeraTxt {
 
     public void extrato(int conta, String msg) throws IOException {
 
+        if(conta==123) {
 
             FileWriter user1 = new FileWriter("Usuario1.txt");
             PrintWriter gravaUser1 = new PrintWriter(user1);
@@ -22,17 +23,15 @@ public class GeraTxt {
             gravaUser1.printf("\n--------------");
 
             user1.close();
+        }
+        else if(conta==124){
 
-        /*
-        if(conta==124){
             FileWriter user2 = new FileWriter("Usuario2.txt");
             PrintWriter gravaUser2 = new PrintWriter(user2);
-
             gravaUser2.printf(msg);
         }
         else
             System.out.println("Conta nao existe");
-        */
     }
 
 }
